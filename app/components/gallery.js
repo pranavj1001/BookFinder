c0c99b209b13266d7a13b21f5d7165f574f6e576
pranavj1001 <pranavj1001@gmail.com>
2017-06-18 12:28:18 +0530

    use the imageThumbnail and link from the api

--- app/components/gallery.js
+++ app/components/gallery.js
@@ -2,12 +2,24 @@ import React, { Component} from 'react';
 
 class Gallery extends Component{
   render(){
+
+    let noBookCoverFound = "https://onlinebookclub.org/book-covers/no-cover.jpg";
+
     return(
       <div>{
         this .props.items.map((item, index) => {
-          let {title} = item.volumeInfo;
+          let {title, imageLinks, infoLink} = item.volumeInfo;
           return(
-            <div key={index}>{title}</div>
+            <div key={index} className = "book">
+              <img
+                src={imageLinks !== undefined? imageLinks.thumbnail : noBookCoverFound}
+                alt="book thumbnail"
+                className = "bookImg"
+              />
+              <div className = "bookText">
+                {title}
+              </div>
+            </div>
           )
         })
       }</div>
